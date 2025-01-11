5fa9aad7a19e711763982bc04252faedfe82202d
HUNG <hungphangialai11@gmail.com>
2025-01-11 22:24:24 +0700

    day 3: learn bridge design pattern through Bank-Account example

---
+++ src/structural/adapter/Bridge.java
@@ -0,0 +1,54 @@
+package structural.adapter;
+
+/**
+ * Bridge design pattern decouple an abstraction from its implementation
+ * so that the two can vary (change) independently
+ */
+public class Bridge {
+    public static void main(String[] args) {
+        Bank vietcomBank = new VietcomBank(new SavingAccount());
+        Bank BIDV = new BIDV(new InvestAccount());
+        vietcomBank.openAccount();
+        BIDV.openAccount();
+    }
+}
+abstract class Bank {
+    Account account;
+    Bank (Account account) {
+        this.account = account;
+    }
+    public abstract void openAccount();
+}
+interface Account {
+    String openAccount();
+}
+class BIDV extends Bank{
+    BIDV(Account account) {
+        super(account);
+    }
+    @Override
+    public void openAccount() {
+        System.out.println("BIDV: open new " + account.openAccount());
+    }
+}
+class VietcomBank extends Bank{
+    VietcomBank(Account account) {
+        super(account);
+    }
+    @Override
+    public void openAccount() {
+        System.out.println("VietcomBank: open new " + account.openAccount());
+    }
+}
+class SavingAccount implements Account {
+    @Override
+    public String openAccount() {
+        return "saving account";
+    }
+}
+class InvestAccount implements Account {
+    @Override
+    public String openAccount() {
+        return "invest account";
+    }
+}
